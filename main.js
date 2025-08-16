b7aa00454283aba629d06b8de9069b1537598b6e
zidanncihuyy <adityazidan400@gmail.com>
2025-08-16 07:41:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["Joestar★", "Cihuyyy"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
